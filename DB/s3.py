3345738398a697d8612385f48e04b2b48629e5ae
mingi KIM <rlaalsrl0302@daum.net>
2023-12-16 16:53:43 +0900

    s3

--- DB/s3.py
+++ DB/s3.py
@@ -4,20 +4,23 @@ from botocore.client import Config
 ACCESS_KEY_ID, ACCESS_SECRET_KEY, BUCKET_NAME, REGION = s3_config()
 
 # client 얻기
-s3 = boto3.client(
-        service_name = "s3",
-        aws_access_key_id = ACCESS_KEY_ID,
-        aws_secret_access_key = ACCESS_SECRET_KEY,
-        region_name = REGION,
-        config=Config(signature_version = 's3v4')
-    )
+def get_client():
+    return boto3.client(
+            service_name = "s3",
+            aws_access_key_id = ACCESS_KEY_ID,
+            aws_secret_access_key = ACCESS_SECRET_KEY,
+            region_name = REGION,
+            config=Config(signature_version = 's3v4')
+        )
 
 # 이미지 불러오기
 def s3_download_img(img_name):
+    s3 = get_client()
     return s3.get_object(Bucket = BUCKET_NAME, Key = img_name)
 
 # s3 업로드
 def s3_upload_img(file, img_name):
+    s3 = get_client()
     s3.Bucket(BUCKET_NAME).put_object(
         Key = img_name, Body = file, ContentType = "image/jpg"
     )
